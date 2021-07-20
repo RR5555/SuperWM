ab3c32569fe8df955b7bb54c6298a1734516f68b
Pranay S. Yadav <mail@pranaysy.com>
2021-07-20 14:33:17 +0530

    Initialize function library

---
+++ psy_process_lib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script contains functions for processing fMRI data from the HCP2021 dataset for the
+Working Memory task, curated for the NMA-CN 2021 session. The functions are to be used
+as individual steps of processing in a script - see corresponding psy_process_runner.py
+for such a pipeline.
+
+@author: Pranay S. Yadav
+"""
